b42e521890ce3349d2018c2a8e89baecea42f86e
Rian <riangabrieldev@gmail.com>
2024-12-20 18:42:48 -0600

    Rm: remove to teste

--- HorseMart/main.js
+++ HorseMart/main.js
@@ -4,35 +4,41 @@ const path = require('path');
 const app = express();
 const PORT = 8000;
 
-// Define o diretório público onde os arquivos HTML estão localizados
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rota para a página inicial (Home)
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Rota para a página de cursos
-app.get('/cursos', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'cursos.html'));
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'blog.html'));
 });
 
-// Rota para a página de professores
-app.get('/professores', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'professores.html'));
-});
-
-// Rota para a página Sobre
-app.get('/sobre', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'sobre.html'));
-});
-
-// Rota para a página Contato
 app.get('/contato', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'contato.html'));
 });
 
-// Inicia o servidor
+app.get('/cursos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'cursos.html'));
+});
+
+app.get('/depoimentos', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'depoimentos.html'));
+});
+
+app.get('/faq', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'FAQ.html'));
+});
+
+app.get('/professores', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'professores.html'));
+});
+
+app.get('/sobre', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'sobre.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
+
